6862297a3dec38c4d10f62e8cdb3619c39dcba05
Leturgone <finekon221@gmail.com>
2024-03-05 23:49:29 +0300

    Задание 6

--- zd6.py
+++ zd6.py
@@ -1,19 +1,3 @@
-# import math
-
-
-# # def main(input_set):
-# #     # Calculate O
-# #     oO = {math.ceil(p / 9) for p in input_set if not -79 < p < 67}
-# #     # Calculate I
-# #     iI = {p - p % 2 for p in input_set if -99999999999999999999 < p <= 34}
-# #     # Calculate Z
-# #     zZ = {5 * o - E ** 2 for o in oO for E in iI if o > E}
-    
-# #     sum1 = sum(math.fabs(E) for E in iI)
-# #     sum2 = sum(E * C for E in iI for C in zZ)
-# #     res = int(sum1 - sum2)
-# #     return res
-
 from math import fabs, floor
 
 
