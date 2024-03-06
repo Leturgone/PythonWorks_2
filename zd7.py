289b8e663e21ea06057bd638a17103074e20b536
Leturgone <finekon221@gmail.com>
2024-03-06 16:44:03 +0300

    Все варианты решения

--- zd7.py
+++ zd7.py
@@ -172,7 +172,47 @@
 
 #Пятый способ
 
+class tree():
+
+    def __init__(self, number, top, topcon, mid, midcon, down, downcon):
+        self.number = number
+        self.top = top
+        self.mid = mid
+        self.down = down
+        self.topCon = topcon
+        self.midCon = midcon
+        self.downCon = downcon
+
+    def find(self, mas: list, ):
+        if self.top == mas[self.number]:
+            if (type(self.topCon) == int):
+                return self.topCon
+            return self.topCon.find(mas)
+        if self.mid == mas[self.number]:
+            if (type(self.midCon) == int):
+                return self.midCon
+            return self.midCon.find(mas)
+        if self.down == mas[self.number]:
+            if (type(self.downCon) == int):
+                return self.downCon
+            return self.downCon.find(mas)
+
+
+def main(mas):
+    x1 = tree(1, 1991, 1, 1968, 2, 1989, 3)
+    x41 = tree(1, 1991, 5, 1968, 6, 1989, 7)
+    x34 = tree(4, 1973, 8, None, None, 1966, 9)
+    x3 = tree(3, 2007, 0, None, None, 2019, x1)
+    x4 = tree(4, 1973, 4, None, None, 1966, x41)
+    x03 = tree(3, 2007, x34, None, None, 2019, 10)
+    x0 = tree(0, "OPAL", x3, "ELM", x4, "ZIMPL",x03)
+    x2 = tree(2, "EAGLE", x0,"SLASH", 11, "CSV", 12)
+    return x2.find(mas)
+
+
 
 print(main(['OPAL', 1991, 'SLASH', 2019, 1973]))
 print(main(['OPAL', 1968, 'CSV', 2007, 1966]))
-print(main(['OPAL', 1968, 'EAGLE', 2019, 1966]))
+print(main(['OPAL', 1968, 'EAGLE', 2019, 1966])) 
+print(main(['ELM', 1991, 'EAGLE', 2007, 1966])) 
+print(main(['ZIMPL', 1968, 'EAGLE', 2019, 1966]))
