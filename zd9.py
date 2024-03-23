fea9b88738fdf2a774f813aaebbaf086f3f9daec
Leturgone <finekon221@gmail.com>
2024-03-23 17:08:17 +0300

    Найдено 2 по популярности решение

--- zd9.py
+++ zd9.py
@@ -1,19 +1,41 @@
+
+
+
+
+
+
+#Второе по популярности решение
 def main(s):
-    key_words = ["do let", "done;","<: "," :>", "::=", "."]
+    k = ["do", "let", "ne;", "<: ", " :>", "::=", ".", "\n"]
     list_of_tuples = []
     input_string = s
-    input_string = input_string.replace(key_words[0],'').replace(key_words[1],'')
-    input_string = input_string.replace(key_words[2],'').replace(key_words[3],'')
-    input_string = input_string.replace(key_words[4],'').replace(key_words[5],'').split(' ')
-    
+    input_string = input_string.replace(k[0], " ").replace(k[1], " ")
+    input_string = input_string.replace(k[2], " ").replace(k[3], " ")
+    input_string = input_string.replace(k[4], " ").replace(k[5], " ")
+    input_string = input_string.replace(k[6], " ").replace(k[7], " ")
+    input_string = input_string.split(" ")
+    new_list = []
     for i in input_string:
-        if [i]!= ' ':
-            
-            
-    # result = [(pairs[i], int(pairs[i+1])) for i in range(0, len(pairs), 2)]
-    return input_string
+        if i != "":
+            new_list.append(i)
+    for i in range(0, len(new_list) - 1, 2):
+        list_of_tuples.append((new_list[i], int(new_list[i + 1])))
+    return list_of_tuples
+
+
+
+
+
 
 input_string = "<: do let soat ::= 5521. done;do let isorge ::= 1596. done;do let gearxe_947 ::=-1944. done; :>"
+input_string2 = "<: do let ente::= 496. done; do let gequ ::= -5474. done; :>"
+input_string3 = '<: do let soat ::= 5521. done;do let isorge ::= 1596. done;do let\ngearxe_947 ::=-1944. done; :>'
+input_string4 = '<: do let cesoin ::= -8045. done; do let enus ::= 5595. done; :>'
+input_string5 = '<: do let gear_736 ::= -3814. done; do let anbe ::=5132. done; do let\nerinza_449 ::= 500. done;do let riri_876::=1021. done;:>'
+input_string6 = '<:do let marabe_7 ::=-1644. done; do let eren_319 ::=6806. done; do\nlet zaleza ::= 6167. done;:>'
 print(main(input_string))
-
-#[('ente', 496), ('gequ', -5474)]
+print(main(input_string2))
+print(main(input_string3))
+print(main(input_string4))
+print(main(input_string5))
+print(main(input_string6))
