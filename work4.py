681c7d5195def4fb9f4f5d1b953ec067384a4537
Leturgone <finekon221@gmail.com>
2024-03-30 13:42:09 +0300

    работа

--- work4.py
+++ work4.py
@@ -770,71 +770,121 @@
 
 import operator
 
-OP_NAMES = {0: 'push', 1: 'op', 2: 'call', 3: 'is', 4: 'to', 5: 'exit'}
+# OP_NAMES = {0: 'push', 1: 'op', 2: 'call', 3: 'is', 4: 'to', 5: 'exit'}
 
-def not_implemented(vm):
-    raise RuntimeError('Not implemented!')
+# def not_implemented(vm):
+#     raise RuntimeError('Not implemented!')
 
-LIB = { # Для быстрого задания большинства операций полезен модуль operator
-    '+': operator.add,
-    '-': operator.sub,
-    '*': operator.mul,
-    '/': operator.floordiv, # Целочисленный вариант деления
-    '%': operator.mod,
-    '&':  operator.and_,
-    '|': operator.or_,
-    '^': operator.xor,
-    '<': operator.lt,
-    '>': operator.gt,
-    '=': operator.eq,
-    '<<': operator.lshift,
-    '>>': operator.rshift,
-    'if': not_implemented,
-    'for': not_implemented,
-    '.': not_implemented,
-    'emit': not_implemented,
-    '?': not_implemented,
-    'array': not_implemented,
-    '@': not_implemented,
-    '!': not_implemented
-}
+# LIB = { # Для быстрого задания большинства операций полезен модуль operator
+#     '+': operator.add,
+#     '-': operator.sub,
+#     '*': operator.mul,
+#     '/': operator.floordiv, # Целочисленный вариант деления
+#     '%': operator.mod,
+#     '&':  operator.and_,
+#     '|': operator.or_,
+#     '^': operator.xor,
+#     '<': operator.lt,
+#     '>': operator.gt,
+#     '=': operator.eq,
+#     '<<': operator.lshift,
+#     '>>': operator.rshift,
+#     'if': not_implemented,
+#     'for': not_implemented,
+#     '.': lambda vm: print(vm.stack.pop()),
+#     'emit': not_implemented,
+#     '?': not_implemented,
+#     'array': not_implemented,
+#     '@': not_implemented,
+#     '!': not_implemented
+# }
 
 
 
-# def disasm(code):
-#     pc = 0
-#     while pc < len(code):
-#         op = code[pc]
-#         pc += 1
-#         if op == 0: # PUSH
-#             val = code[pc]
-#             pc += 1
-#             print('  push', val)
-#         elif op == 1: # OP
-#             val = code[pc]
-#             pc += 1
-#             print('  op', chr(val))
-#         elif op == 2: # CALL
-#             val = code[pc]
-#             pc += 1
-#             print('  call', val)
-#         elif op == 3: # IS
-#             print('  is')
-#         elif op == 4: # TO
-#             print('  to')
-#         elif op == 5: # EXIT
-#             val = code[pc]
-#             pc += 1
-#             print('  exit', val)
-#         else:
-#             print('Неизвестная операция:', op)
-# disasm([0, 16, 16, 1, 121, 5])
-# # Пример использования:
-# # >>> disasm([0, 16, 16, 1, 121, 5])
-# # entry:
-# #   push 2
-# #   push 2
-# #   op '+'
-# #   op '.'
-# #   exit 0
+# class VM:
+#     def __init__(self, code):
+#         self.stack = []
+#         self.code = code
+#         self.pc = 0
+#     def run(self):
+#         while self.pc < len(self.code):
+#             op = self.code[self.pc]
+#             self.pc += 1
+#             if op in LIB:
+#                 LIB[op]()
+#             else:
+#                 print("Invalid opcode:", op)
 
+# def test_VM():
+#     bytecode = [0, 2, 0, 2, 1, 2] 
+#     vm = VM(bytecode)
+#     vm.run()
+
+# test_VM()
+
+LIB = [
+    '+', '-', '*', '/', '%', '&', '|', '^', '<', '>', '=', '<<', '>>', 'if',
+    'for', '.', 'emit', '?', 'array', '@', '!'
+]
+
+class VM:
+    def __init__(self, code):
+        self.stack = []
+        self.code = code
+        self.pc = 0
+        self.scope = {} 
+        self.call_stack = []
+
+    def run(self):
+        while self.pc < len(self.code):
+            instruction = self.code[self.pc]
+            op_code = instruction & 0b111
+            arg = instruction >> 3
+            
+            if op_code == 0:
+                self.stack.append(arg)
+                self.pc += 1
+            elif op_code == 1:
+                if arg < len(LIB):
+                    operation = LIB[arg]
+                    if operation == '+':
+                        a = self.stack.pop()
+                        b = self.stack.pop()
+                        self.stack.append(a + b)
+                    elif operation == '.':
+                        print(self.stack.pop())
+                    elif operation == 'emit':
+                        print(chr(self.stack.pop()), end='')
+                self.pc += 1
+            elif op_code == 2:
+                function_address = self.scope.get(arg, (0,0))[1]
+                if function_address:
+                    self.call_stack.append(self.pc)
+                    self.pc = function_address
+                else:
+                    print("Function not found")
+                    break
+            elif op_code == 3:
+                self.scope[arg] = ('function', self.pc + 1)
+                self.pc += 1
+            elif op_code == 5:
+                if self.call_stack:
+                    self.pc = self.call_stack.pop() + 1
+                else:
+                    break
+            else:
+                print("Unknown operation")
+                break
+
+LIB.extend(['+', '.', 'emit'])
+
+bytecode = [
+    57, 8440, 129, 8704, 129, 8688, 129, 8600, 129, 8704, 129, 8576, 129, 8672,
+    129, 8672, 129, 8576, 129, 256, 129, 8728, 129, 8712, 129, 8696, 129, 8616,
+    129, 8768, 129, 8680, 129, 8688, 129, 256, 129, 8592, 129, 8792, 129, 8696,
+    129, 8688, 129, 8664, 129, 8680, 129, 8616, 129, 8680, 129, 8576, 129, 264,
+    129, 5, 0, 3, 2, 5
+]
+
+vm = VM(bytecode)
+vm.run()
